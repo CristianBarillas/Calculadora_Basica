8f8cf02ac67e938c98c1ba85e28d8d16da1b1fb3
Cristian <cristianbarillas23@gmail.com>
2024-02-25 12:52:03 -0600

    Clase cliente

---
+++ OneDrive/Escritorio/Actividad 2 POO/Actividad 2 POO....cpp
@@ -0,0 +1,16 @@
+#include "Persona.cpp"
+#include <iostream>
+using namespace std;
+
+class Cliente : Persona {
+
+	private : string nit;
+	
+	//constructor
+	public : 
+	cliente (){
+	}
+	
+	Cliente (string nom,string ape,string dir,int tel, string n) : Persona(nom,ape,dir,tel) {
+		nit = n;
+
